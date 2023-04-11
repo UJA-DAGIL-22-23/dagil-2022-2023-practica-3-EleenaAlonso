c5cae1a64d39d3f1893c7aa3a6b3ac6af724284c
Elena Alonso Tejederas <eat00004@red.ujaen.es>
2023-04-11 16:42:46 +0200

    Modificaciones en proceso para el TDD de la HU 2

--- ms-plantilla/spec/server-spec.js
+++ ms-plantilla/spec/server-spec.js
@@ -56,51 +56,8 @@ describe('Servidor PLANTILLA:', () => {
         .expect('Content-Type', /json/)
         .expect(function (res) {
           //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
-          for (let i = 0; i < res.body.data.length; i++) {
-            let item = res.body.data[i];
-            assert(res.body.data[i].data.hasOwnProperty('Nombre_completo'));
-            assert(res.body.data[i].data.Nombre_completo.Nombre === item.data.Nombre_completo.Nombre);
-            //console.log(item.data);
-
-          }
-
-          const p = {
-            data: {
-              Nombre_completo: {
-                Nombre: 'Juan',
-                Apellidos: 'Perez'
-              },
-              Fecha: {
-                dia: '1',
-                mes: '2',
-                año: '2000'
-              },
-              Direccion: {
-                calle: 'Calle Falsa',
-                localidad: 'Ciudad Falsa',
-                provincia: 'Provincia Falsa',
-                pais: 'Pais Falso'
-              },
-              Anios_participacion_en_mundial: '4',
-              Num_participaciones_mundiales_JJOO: '2',
-              Mejor_estilo_natacion: 'Estilo libre'
-            },
-            ref: {
-              '@ref': {
-                id: '123456'
-              }
-            }
-          };
-          expect(Plantilla.cuerpoTr(p)).toBe(`<tr title="123456">
-              <td>Juan</td>
-              <td>Perez</td>
-              <td>1/2/2000</td>
-              <td>Calle Falsa, Ciudad Falsa, Provincia Falsa, Pais Falso</td>
-              <td>4</td>
-              <td>2</td>
-              <td>Estilo libre</td>
-              </tr>`);
-
+          assert(res.body.data[0].data.hasOwnProperty('Nombre_completo'));
+          assert(res.body.data[0].data.Nombre_completo.Nombre === 'Mireia');
         })
         .end((error) => { error ? done.fail(error) : done(); }
         );
